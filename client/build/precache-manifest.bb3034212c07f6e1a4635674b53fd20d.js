fad837d18be74ea9ffcf0b04c2521bdcf2ab0d84
iLikeSpaghetti0837 <90133763+macelivity@users.noreply.github.com>
2023-04-18 18:38:01 +0200

    build

---
+++ client/build/precache-manifest.bb3034212c07f6e1a4635674b53fd20d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "76435ec299dba751d4e7",
+    "url": "/static/css/main.860cd96d.chunk.css"
+  },
+  {
+    "revision": "76435ec299dba751d4e7",
+    "url": "/static/js/main.9ade788a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b72bd6ef09874d145a46",
+    "url": "/static/js/2.4dd1553e.chunk.js"
+  },
+  {
+    "revision": "c122182d79b89e4fe346bce8c84f4893",
+    "url": "/index.html"
+  }
+];
